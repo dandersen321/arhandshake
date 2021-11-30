494f545b75c3fbbc9fd2ae189bb77e6c43fdf1eb
Dylan Andersen <dander7@hotmail.com>
2021-11-30 13:45:18 -0600

    can do grabbing count

--- Assets/Scripts/SessionStatusTracking.cs
+++ Assets/Scripts/SessionStatusTracking.cs
@@ -8,31 +8,51 @@ public class SessionStatusTracking : MonoBehaviour
 {
 
     private TMPro.TextMeshProUGUI sessionStatusText;
+    private int count = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         sessionStatusText = this.GetComponent<TMPro.TextMeshProUGUI>();
         sessionStatusText.text = "Hello";
+        ManomotionManager.OnManoMotionFrameProcessed += handleMotionFrame;
         //ARSession.stateChanged += HandleStateChanged;
     }
 
-    // Update is called once per frame
-    void Update()
+    void handleMotionFrame()
     {
-        try {
-            if (ManomotionManager.Instance.Hand_infos.Length > 0)
+        if (ManomotionManager.Instance.Hand_infos.Length > 0)
+        {
+           HandInfoUnity handInfoUnity = ManomotionManager.Instance.Hand_infos[0];
+           TrackingInfo trackingInfo = handInfoUnity.hand_info.tracking_info;
+           GestureInfo gestureInfo = handInfoUnity.hand_info.gesture_info;
+            if (gestureInfo.mano_gesture_trigger == ManoGestureTrigger.GRAB_GESTURE)
             {
-                HandInfoUnity handInfoUnity = ManomotionManager.Instance.Hand_infos[0];
-                sessionStatusText.text = handInfoUnity.hand_info.gesture_info.mano_class.ToString();
-            } else
-            {
-                sessionStatusText.text = "No State atm";
+                sessionStatusText.text = "Grab" + count++;
+            } else {
+                sessionStatusText.text = "Nothing -" + gestureInfo.mano_gesture_trigger + count;
             }
-        } catch(Exception ex) {
-            sessionStatusText.text = ex.Message;
+        } else {
+            sessionStatusText.text = "In else";
         }
     }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+    //     try {
+    //         if (ManomotionManager.Instance.Hand_infos.Length > 0)
+    //         {
+    //             HandInfoUnity handInfoUnity = ManomotionManager.Instance.Hand_infos[0];
+    //             sessionStatusText.text = handInfoUnity.hand_info.gesture_info.mano_class.ToString();
+    //         } else
+    //         {
+    //             sessionStatusText.text = "No State atm";
+    //         }
+    //     } catch(Exception ex) {
+    //         sessionStatusText.text = ex.Message;
+    //     }
+    // }
 
     //private void HandleStateChanged(ARSessionStateChangedEventArgs stateEventArguments)
     //{
