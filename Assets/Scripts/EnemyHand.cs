a66d3422073607ba9b22275c8dafe11bbed0f756
Dylan Andersen <dander7@hotmail.com>
2021-11-30 18:37:05 -0600

    tried a bunch of ar foundation image tracking with no luck

--- Assets/Scripts/EnemyHand.cs
+++ Assets/Scripts/EnemyHand.cs
@@ -105,3 +105,138 @@ public class EnemyHand : MonoBehaviour
         cubeRenderer.sharedMaterial = arCubeMaterial[0];
     }
 }
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class EnemyHand : MonoBehaviour
+// {
+//     private ManoGestureContinuous grab;
+//     private ManoGestureContinuous pinch;
+//     private ManoGestureTrigger click;
+
+//     private Transform originalParentTransform;
+//     private Vector3 originalPosition;
+
+//     private GameObject playerHand;
+
+//     private bool handshakeStarted = false;
+
+//     //[SerializeField]
+//     //private Material[] arCubeMaterial;
+//     [SerializeField]
+//     private GameObject smallCube;
+
+//     private string handTag = "Player";
+//     private Renderer cubeRenderer;
+
+//     void Start()
+//     {
+//         Initialize();
+//     }
+
+//     private void Initialize()
+//     {
+//         grab = ManoGestureContinuous.CLOSED_HAND_GESTURE;
+//         pinch = ManoGestureContinuous.HOLD_GESTURE;
+//         click = ManoGestureTrigger.CLICK;
+//         cubeRenderer = GetComponent<Renderer>();
+//         originalParentTransform = this.transform.parent;
+//         originalPosition = this.transform.position;
+//         //cubeRenderer.sharedMaterial = Material.Create("");
+//         //cubeRenderer.material = arCubeMaterial[0];
+//     }
+
+//     private void Update() {
+//         if(handshakeStarted) {
+//             if(ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.mano_gesture_continuous == grab) {
+//                 transform.parent = playerHand.transform;
+//                 transform.position = ManomotionManager.Instance.Hand_infos[0].hand_info.tracking_info.palm_center;
+//             } else {
+//                 transform.parent = originalParentTransform;
+//                 transform.position = originalPosition;
+//                 handshakeStarted = false;
+//             }
+//         }
+//     }
+
+//     /// <summary>
+//     /// 
+//     /// </summary>
+//     /// <param name="other">The collider that stays</param>
+//     private void OnTriggerStay(Collider other)
+//     {
+//         MoveWhenGrab(other);
+//         // RotateWhenHolding(other);
+//         // SpawnWhenClicking(other);
+//     }
+
+//     /// <summary>
+//     /// If grab is performed while hand collider is in the cube.
+//     /// The cube will follow the hand.
+//     /// </summary>
+//     private void MoveWhenGrab(Collider other)
+//     {
+        
+//         if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.mano_gesture_continuous == grab)
+//         {
+//             handshakeStarted = true;
+//             playerHand = other.gameObject;
+//             //transform.parent = other.gameObject.transform;
+//         }
+
+//         // else
+//         // {
+//         //     transform.parent = originalParentTransform;
+//         //     transform.position = originalPosition;
+//         // }
+//     }
+
+//     /// <summary>
+//     /// If pinch is performed while hand collider is in the cube.
+//     /// The cube will start rotate.
+//     /// </summary>
+//     private void RotateWhenHolding(Collider other)
+//     {
+//         if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.mano_gesture_continuous == pinch)
+//         {
+//             transform.Rotate(Vector3.up * Time.deltaTime * 50, Space.World);
+//         }
+//     }
+
+//     /// <summary>
+//     /// If pick is performed while hand collider is in the cube.
+//     /// The cube will follow the hand.
+//     /// </summary>
+//     private void SpawnWhenClicking(Collider other)
+//     {
+//         if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.mano_gesture_trigger == click)
+//         {
+//             Instantiate(smallCube, new Vector3(transform.position.x, transform.position.y + transform.localScale.y / 1.5f, transform.position.z), Quaternion.identity);
+//         }
+//     }
+
+//     /// <summary>
+//     /// Vibrate when hand collider enters the cube.
+//     /// </summary>
+//     /// <param name="other">The collider that enters</param>
+//     private void OnTriggerEnter(Collider other)
+//     {
+//         if (other.gameObject.tag == handTag)
+//         {
+//             cubeRenderer.material.SetColor("green", Color.green);
+//             Handheld.Vibrate();
+//         }
+//     }
+
+//     /// <summary>
+//     /// Change material when exit the cube
+//     /// </summary>
+//     /// <param name="other">The collider that exits</param>
+//     private void OnTriggerExit(Collider other)
+//     {
+//         //cubeRenderer.sharedMaterial = arCubeMaterial[0];
+//         cubeRenderer.material.SetColor("red", Color.red);
+//     }
+// }
